54049b8ae33c6b5acde738851c83c39343d0e0dd
edenk77 <114165536+edenk77@users.noreply.github.com>
2022-11-30 08:38:47 +0100

    Mon premier commit pour mon portfolio

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    var app;
+    $(document).ready(function () {
+        return app.init();
+    });
+    app = {
+        text: 'Hello! Bienvenue dans mon portfolio ;) ',
+        index: 0,
+        chars: 0,
+        speed: 100,
+        container: '.text .content',
+        init: function () {
+            this.chars = this.text.length;
+            return this.write();
+        },
+        write: function () {
+            $(this.container).append(this.text[this.index]);
+            if (this.index < this.chars) {
+                this.index++;
+                return window.setTimeout(function () {
+                    return app.write();
+                }, this.speed);
+            }
+        }
+    };
+}.call(this));
+
+$(window).load(function(){
+	
+	$(".loader,.loading img").fadeOut(800,function(){
+			$("body").css("overflow","auto");
+				$(".loading").fadeOut(800,function(){
+				$(this).remove();
+			});
+		});
+
+  });

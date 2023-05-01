f822703b84456995695578c6faf828364cade9ac
edenk77 <114165536+edenk77@users.noreply.github.com>
2023-05-01 19:43:34 +0200

    Modification Responsive

--- js/script.js
+++ js/script.js
@@ -34,4 +34,12 @@ $(window).load(function(){
 			});
 		});
 
-  });
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
